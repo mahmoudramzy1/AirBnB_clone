50dc750ba969dd9825997672becc26c57800daf4
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-03-09 05:29:25 -0800

    p

--- console.py
+++ console.py
@@ -3,11 +3,13 @@
 import cmd
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
 
 class HBNBCommand(cmd.Cmd):
     """console class"""
     prompt = '(hbnb)'
     classes = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]    
+
     def do_EOF(self, arg):
         """exit console"""
         return True
--- models/state.py
+++ models/state.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 """module to class state"""
 import models
-from models.base_models import BaseModel
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """subclass from BaseModel"""
+
+    name = ""
