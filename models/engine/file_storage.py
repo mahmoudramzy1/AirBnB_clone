49ab62d90d1e12268d9eba69d7f910a268c73e9f
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-03-10 15:05:32 -0700

    p

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,11 @@ import json
 from models.base_model import BaseModel
 import os
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
 
 
 class FileStorage:
